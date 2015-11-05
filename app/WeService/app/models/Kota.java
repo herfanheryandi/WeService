f0fd295bf45fe4735875bcef9871978d8bf59bc5
herfanheryandi <herfanheryandi@gmail.com>
2015-11-05 17:36:44 +0700

    Menambahkan kelas Kota

---
+++ app/WeService/app/models/Kota.java
@@ -0,0 +1,17 @@
+public class Kota{
+	String nama;
+	String lintang;
+	String bujur;
+	String cuaca;
+	String suhuMax;
+	String suhuMin;
+	
+	public Kota(String nama, String lintang, String bujur, String cuaca, String suhuMax, String suhuMin){
+		this.nama = nama;
+		this.lintang = lintang;
+		this.bujur = bujur;
+		this.cuaca = cuaca;
+		this.suhuMax = suhuMax;
+		this.suhuMin = suhuMin;	
+	}
+}
