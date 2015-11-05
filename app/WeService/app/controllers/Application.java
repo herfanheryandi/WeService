f3dc05a5c9799800a87b881b497ea647d7f84f2a
herfanheryandi <herfanheryandi@gamil.com>
2015-11-05 17:25:21 +0700

    Upload beberapa perubahan

--- app/WeService/app/controllers/Application.java
+++ app/WeService/app/controllers/Application.java
@@ -2,13 +2,31 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import models.Kota;
 import views.html.*;
+import java.io.IOException;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Application extends Controller {
-
-    public Result index() {
-        return ok(index.render("Your new application is ready."));
+	List<Kota> list;
+	
+    public Result index() throws IOException{
+    	Connection conn = Jsoup.connect("http://data.bmkg.go.id/propinsi_13_1.xml");
+        conn.timeout(0);
+        conn.method(Connection.Method.GET);
+        Response resp = conn.execute();
+        Document xml =  resp.parse();
+        Elements elem = xml.select("row");
+        list = new ArrayList<Kota>();
+        for (Element e:elem) {
+            list.add(new Kota(e.child(0).text(),e.child(2).text(),e.child(3).text(),e.child(6).text(),e.child(8).text(),e.child(9).text()));
+        }
+        return ok(index.render(list));
     }
 
 }
