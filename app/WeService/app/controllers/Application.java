ba760ead6deffb3111f7580e2dadc163563eae2b
ferdiyanhamdani <deadlyferz@gmail.com>
2015-11-12 15:20:26 +0700

    Menambahkan halaman Map

--- app/WeService/app/controllers/Application.java
+++ app/WeService/app/controllers/Application.java
@@ -15,7 +15,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Application extends Controller {
-	List<Kota> list;
+	List<Kota> listKota;
 	
     public Result index() throws IOException{
     	Connection conn = Jsoup.connect("http://data.bmkg.go.id/propinsi_13_1.xml");
@@ -24,11 +24,25 @@ public class Application extends Controller {
         Response resp = conn.execute();
         Document xml =  resp.parse();
         Elements elem = xml.select("row");
-        list = new ArrayList<Kota>();
+        listKota = new ArrayList<Kota>();
         for (Element e:elem) {
-            list.add(new Kota(e.child(0).text(),e.child(2).text(),e.child(3).text(),e.child(6).text(),e.child(7).text(),e.child(8).text()));
+            listKota.add(new Kota(e.child(0).text(),e.child(2).text(),e.child(3).text(),e.child(6).text(),e.child(7).text(),e.child(8).text()));
         }
-        return ok(views.html.index.render(list));
+        return ok(views.html.index.render(listKota));
+    }
+    
+    public Result map(String kode) throws IOException{
+    	Connection conn = Jsoup.connect("http://data.bmkg.go.id/propinsi_"+kode+"_1.xml");
+        conn.timeout(0);
+        conn.method(Connection.Method.GET);
+        Response resp = conn.execute();
+        Document xml =  resp.parse();
+        Elements elem = xml.select("row");
+        listKota = new ArrayList<Kota>();
+        for (Element e:elem) {
+            listKota.add(new Kota(e.child(0).text(),e.child(2).text(),e.child(3).text(),e.child(6).text(),e.child(7).text(),e.child(8).text()));
+        }
+        return ok(views.html.index.render(listKota));
     }
 
 }
